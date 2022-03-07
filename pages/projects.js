d766fcbe4499043155a0380a6c52e7813d86aefb
yoni <yoni@kali>
2022-03-07 08:28:00 +0300

    HTC project upload

--- pages/projects.js
+++ pages/projects.js
@@ -25,7 +25,7 @@ function projects() {
       <RightSide />
       <div className = 'w-full'>
       <Homee />
-      <div class = "bg-[#24292e] w-full h-full text-white px-10 flex space-x-4">
+      <div class = "bg-[#24292e] w-full h-full text-white px-10 grid grid-cols-3 space-x-4">
         <div className = 'relative w-[30rem] h-[30rem] group'>
             <Image 
                 src = '/imgs/amazonclone.png'
@@ -49,6 +49,20 @@ function projects() {
             />
             <div className = 'absolute bg-gradient-to-t from-black to-transparent w-[30rem] h-[16rem] top-28 opacity-0 group-hover:opacity-100' />
             <Link href = 'https://airbnb-clone-yoni1342.vercel.app/'>
+              <button className =' bg-[#edf2f7] px-10 py-4 rounded-full shadow-md text-purple-500 my-3 font-bold 
+                  hover:shadow-xl shadow-black active:scale-90 transtion duration-150 absolute top-52 left-36 animate-bounce opacity-0 group-hover:opacity-100'>
+                  View Project  
+              </button>
+            </Link>
+        </div>
+        <div className = 'relative w-[30rem] h-[30rem] group'>
+            <Image
+                src = '/imgs/HTC.png'
+                layout = 'fill'
+                objectFit = 'contain'
+            />
+            <div className = 'absolute bg-gradient-to-t from-black to-transparent w-[30rem] h-[16rem] top-28 opacity-0 group-hover:opacity-100' />
+            <Link href = 'https://gracious-ride-37384a.netlify.app/'>
               <button className =' bg-[#edf2f7] px-10 py-4 rounded-full shadow-md text-purple-500 my-3 font-bold 
                   hover:shadow-xl shadow-black active:scale-90 transtion duration-150 absolute top-52 left-36 animate-bounce opacity-0 group-hover:opacity-100'>
                   View Project  
